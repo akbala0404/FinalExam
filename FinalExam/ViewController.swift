d85a65af5ff57ce2fa16a65bfa62619b69795393
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-05-17 22:22:07 +0600

    Initial Commit

---
+++ FinalExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalExam
+//
+//  Created by Акбала Тлеугалиева on 5/17/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
